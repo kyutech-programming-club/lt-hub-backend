06ce8a877a558965c407a99a88e57c77a2985faf
take-cantik <take.cantik17@gmail.com>
2022-02-06 23:53:12 +0900

    Add: assumed processing comment

--- backend/app/Usecases/EventUsecase.php
+++ backend/app/Usecases/EventUsecase.php
@@ -13,46 +13,80 @@ class EventUsecase implements EventUsecaseInterface
 {
     public function index(GetListDto $getListDto)
     {
-        //
+        /*
+            if accessUserRole == 0
+                getAllEvent
+            else
+                getAllPublicEvent & getParticipantLimitEvent
+        */
     }
 
     public function store(CreateDto $createDto)
     {
-        //
+        /*
+            createEvent
+        */
     }
 
     public function created(BaseDto $dto)
     {
-        //
+        /*
+            getCreatedEvents
+        */
     }
 
     public function joined(BaseDto $dto)
     {
-       //
+        /*
+            getJoinedEvents
+        */
     }
 
     public function mypage(FindByUserIdDto $findByUserIdDto)
     {
-        //
+        /*
+            if role == 0
+                getAllUserPaticipantEvent(userId)
+            else
+                getPublicUserParticipantEvent
+        */
     }
 
     public function show(FindByIdDto $findByIdDto)
     {
-        //
+        /*
+            // 保留
+        */
     }
 
     public function update(UpdateDto $updateDto)
     {
-        //
+        /*
+            if accessUser == hostId
+                UpdateEvent
+        */
     }
 
     public function destroy(FindByIdDto $findByIdDto)
     {
-        //
+        /*
+            if accessUser == hostId
+                deleteEvent
+                deleteEventTalks
+        */
     }
 
     public function detail(FindByIdDto $findByIdDto)
     {
-        //
+        /*
+            if role == 1
+                getEventDetail
+            else
+                getParticipantState
+                if (state == ok)
+                    getEventDetail
+                else
+                    getLimitEvent
+        */
     }
 }
