ad5b27ffeb306efd3b598c51b99c0e1bf4f2b1a4
take-cantik <take.cantik17@gmail.com>
2022-02-07 14:10:42 +0900

    Fix: change name of event repository method

---
+++ backend/app/Repositories/EventRepository.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Http\Dto\Event\CreateDto;
+use App\Http\Dto\Event\UpdateDto;
+
+class EventRepository implements EventRepositoryInterface
+{
+    public function getAllEvents()
+    {
+        //
+    }
+
+    public function getBrowsableEvents(int $userId)
+    {
+        //
+    }
+
+    public function createEvent(CreateDto $createDto)
+    {
+        //
+    }
+
+    public function getCreatedEvents(int $userId)
+    {
+        //
+    }
+
+    public function getJoinedEvents(int $userId)
+    {
+        //
+    }
+
+    public function getAllParticipantEvents(int $userId)
+    {
+        //
+    }
+
+    public function getBrowsableParticipantEvents(int $userId)
+    {
+        //
+    }
+
+    public function updateEvent(UpdateDto $updateDto)
+    {
+        //
+    }
+
+    public function deleteEvent(int $eventId)
+    {
+        //
+    }
+
+    public function getEventDetail(int $eventId)
+    {
+        //
+    }
+
+    public function getParticipantState(int $eventId, int $userId)
+    {
+        //
+    }
+
+    public function getLimitEvent(int $userId)
+    {
+        //
+    }
+}
--- backend/app/Repositories/EventRepositoryInterface.php
+++ backend/app/Repositories/EventRepositoryInterface.php
@@ -7,12 +7,12 @@ use App\Http\Dto\Event\UpdateDto;
 
 interface EventRepositoryInterface {
     public function getAllEvents();
-    public function getPublicEvents();
+    public function getBrowsableEvents(int $userId);
     public function createEvent(CreateDto $createDto);
     public function getCreatedEvents(int $userId);
     public function getJoinedEvents(int $userId);
     public function getAllParticipantEvents(int $userId);
-    public function getPublicParticipantEvents(int $userId);
+    public function getBrowsableParticipantEvents(int $userId);
     public function updateEvent(UpdateDto $updateDto);
     public function deleteEvent(int $eventId);
     public function getEventDetail(int $eventId);
