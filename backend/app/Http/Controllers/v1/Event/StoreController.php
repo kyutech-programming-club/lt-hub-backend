c715abb7f95a4e267dd5231d65c15bb0b9b9db5c
take-cantik <take.cantik17@gmail.com>
2022-02-08 00:45:55 +0900

    Add: store controller

---
+++ backend/app/Http/Controllers/v1/Event/StoreController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\v1\Event;
+
+use App\Http\Controllers\Controller;
+use App\Usecases\EventUsecase;
+use App\Http\Requests\Event\CreateRequest;
+use App\Http\Dto\Event\CreateDto;
+
+class StoreController extends Controller
+{
+    public function __invoke(CreateRequest $request, EventUsecase $usecase)
+    {
+        $createDto = new CreateDto([
+            'accessUserId' => $request->accessUserId,
+            'accessUserRole' => $request->accessUserRole,
+            'title' => $request->title,
+            'description' => $request->description,
+            'startTime' => $request->startTime,
+            'endTime' => $request->endTime,
+            'location' => $request->location,
+            'state' => $request->state
+        ]);
+
+        $result = $usecase->store($createDto);
+    }
+}
