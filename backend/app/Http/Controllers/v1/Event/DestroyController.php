52e2a77c633b6b4ef1fccc3c26e86b9a823a619f
take-cantik <take.cantik17@gmail.com>
2022-02-08 18:44:07 +0900

    Add: update and destroy usecase

--- backend/app/Http/Controllers/v1/Event/DestroyController.php
+++ backend/app/Http/Controllers/v1/Event/DestroyController.php
@@ -4,13 +4,13 @@ namespace App\Http\Controllers\v1\Event;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Usecases\EventUsecase;
+use App\Usecases\Event\DestroyUsecase;
 use App\Http\Dto\Event\FindByIdDto;
 use Vinkla\Hashids\Facades\Hashids;
 
 class DestroyController extends Controller
 {
-    public function __invoke(Request $request, string $eventId, EventUsecase $usecase)
+    public function __invoke(Request $request, string $eventId, DestroyUsecase $usecase)
     {
         $findByIdDto = new FindByIdDto([
             'accessUserId' => $request->accessUserId,
--- backend/app/Http/Controllers/v1/Event/UpdateController.php
+++ backend/app/Http/Controllers/v1/Event/UpdateController.php
@@ -4,13 +4,13 @@ namespace App\Http\Controllers\v1\Event;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Event\UpdateRequest;
-use App\Usecases\EventUsecase;
+use App\Usecases\Event\UpdateUsecase;
 use App\Http\Dto\Event\UpdateDto;
 use Vinkla\Hashids\Facades\Hashids;
 
 class UpdateController extends Controller
 {
-    public function __invoke(UpdateRequest $request, string $eventId, EventUsecase $usecase)
+    public function __invoke(UpdateRequest $request, string $eventId, UpdateUsecase $usecase)
     {
         $updateDto = new UpdateDto([
             'accessUserId' => $request->accessUserId,
@@ -23,6 +23,6 @@ class UpdateController extends Controller
             'state' => $request->state
         ]);
 
-        $result = $usecase->update($updateDto);
+        $result = $usecase->execute($updateDto);
     }
 }
