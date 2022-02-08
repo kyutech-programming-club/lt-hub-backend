b5a460a3005ceba30a23900c21f8cf3141b6d4ca
take-cantik <take.cantik17@gmail.com>
2022-02-08 18:25:47 +0900

    Add: JoinedUsecase

--- backend/app/Http/Controllers/v1/Event/JoinedController.php
+++ backend/app/Http/Controllers/v1/Event/JoinedController.php
@@ -4,12 +4,12 @@ namespace App\Http\Controllers\v1\Event;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Usecases\EventUsecase;
+use App\Usecases\Event\JoinedUsecase;
 use App\Http\Dto\BaseDto;
 
 class JoinedController extends Controller
 {
-    public function __invoke(Request $request, EventUsecase $usecase)
+    public function __invoke(Request $request, JoinedUsecase $usecase)
     {
         $dto = new BaseDto([
             'accessUserId' => $request->accessUserId,
