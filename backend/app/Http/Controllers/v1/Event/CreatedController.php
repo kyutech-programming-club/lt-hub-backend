4bbf038dc9d72abcb4a0161a3c2fed9ad87bd1be
take-cantik <take.cantik17@gmail.com>
2022-02-08 18:23:12 +0900

    Add: created usecase

--- backend/app/Http/Controllers/v1/Event/CreatedController.php
+++ backend/app/Http/Controllers/v1/Event/CreatedController.php
@@ -3,13 +3,13 @@
 namespace App\Http\Controllers\v1\Event;
 
 use App\Http\Controllers\Controller;
-use App\Usecases\EventUsecase;
 use Illuminate\Http\Request;
 use App\Http\Dtos\BaseDto;
+use App\Usecases\Event\CreatedUsecase;
 
 class CreatedController extends Controller
 {
-    public function __invoke(Request $request, EventUsecase $usecase)
+    public function __invoke(Request $request, CreatedUsecase $usecase)
     {
         $dto = new BaseDto([
             'accessUserId' => $request->accessUserId,
