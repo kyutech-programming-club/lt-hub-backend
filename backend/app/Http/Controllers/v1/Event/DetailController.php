1f0fccacc2f2b76c1e65e5b665b7f25867b272ec
take-cantik <take.cantik17@gmail.com>
2022-05-26 15:23:02 +0900

    Update: divide controller

---
+++ backend/app/Http/Controllers/v1/Event/DetailController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\v1\Event;
+
+use App\Http\Controllers\Controller;
+use App\Usecases\EventUsecase;
+use Illuminate\Http\Request;
+use App\Http\Dtos\Event\FindByIdDto;
+use Vinkla\Hashids\Facades\Hashids;
+
+class DetailController extends Controller
+{
+    public function __invoke(Request $request, string $eventId, EventUsecase $usecase)
+    {
+        $findByIdDto = new FindByIdDto([
+            'accessUserId' => $request->accessUserId,
+            'accessUserRole' => $request->accessUserRole,
+            'eventId' => Hashids::decode($eventId)[0]
+        ]);
+
+        $result = $this->eventUsecase->destroy($findByIdDto);
+    }
+}
